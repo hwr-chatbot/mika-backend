a17b0aa376a291345fa7ebef8b6b0e4fa32d05d1
Floxyi <florianmeik.winkler@gmail.com>
2025-06-09 18:30:24 +0200

    deactivate currently unused actions#

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "rasa-backend",
+      name: "mika-backend",
       script: "./scripts/start-rasa-backend.sh",
       interpreter: "bash",
       cwd: "/home/s_winklerf23/apps/mika-backend",
     },
-    {
-      name: "rasa-actions",
-      script: "./scripts/start-rasa-actions.sh",
-      interpreter: "bash",
-      cwd: "/home/s_winklerf23/apps/mika-backend",
-    },
+    // {
+    //   name: "mika-actions",
+    //   script: "./scripts/start-rasa-actions.sh",
+    //   interpreter: "bash",
+    //   cwd: "/home/s_winklerf23/apps/mika-backend",
+    // },
   ],
 };
