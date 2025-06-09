4f66a5cf35e19ffb8726a34cd20038922a3d3b31
Floxyi <florianmeik.winkler@gmail.com>
2025-06-09 18:34:02 +0200

    use correct cors config

--- scripts/start-rasa-actions.sh
+++ scripts/start-rasa-actions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec poetry run rasa run actions --cors "http://localhost:5173"
+exec poetry run rasa run actions --cors "http://mika.lehre.hwr-berlin.de:5173"
--- scripts/start-rasa-backend.sh
+++ scripts/start-rasa-backend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec poetry run rasa run --enable-api --cors "http://localhost:5173"
+exec poetry run rasa run --enable-api --cors "http://mika.lehre.hwr-berlin.de:5173"
